464c5dbbdfcf54698836462c570a83df0684ea96
Patrick Kolodgy <pkolodgy@gmail.com>
2017-04-09 20:36:50 -0400

    Add MUV to handle fake JSON

--- Main.elm
+++ Main.elm
@@ -1,47 +1,109 @@
-module Hello exposing (..)
+module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (class, target, href, property)
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (required)
+import Data
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    body []
-        [ h1 []
-            [ text ("Patrick Kolodgy") ]
-        , p []
-            [ text ("Web developer / Musician / Audio engineer / Podcaster") ]
-        , p []
-            [ a
-                [ href "https://github.com/pkolo"
-                , target "_blank"
-                ]
-                [ text ("github") ]
-            , text (" / ")
-            , a
-                [ href "https://www.linkedin.com/in/pkolodgy/"
-                , target "_blank"
-                ]
-                [ text ("linkedin") ]
+    Html.beginnerProgram
+        { view = view
+        , model = initialModel
+        , update = update
+        }
+
+
+
+-- model
+
+
+type alias Model =
+    { results : List SearchResult
+    }
+
+
+type alias SearchResult =
+    { id : Int
+    , name : String
+    , timeline : String
+    , status : String
+    , categories : String
+    , technologies : String
+    , link : String
+    , src_link : String
+    , description : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { results = decodeResults Data.json
+    }
+
+
+decodeResults : String -> List SearchResult
+decodeResults json =
+    case decodeString responseDecoder json of
+        Ok searchResults ->
+            searchResults
+
+        Err errorMessage ->
+            []
+
+
+responseDecoder : Decoder (List SearchResult)
+responseDecoder =
+    Json.Decode.Pipeline.decode identity
+        |> required "projects" (list searchResultDecoder)
+
+
+searchResultDecoder : Decoder SearchResult
+searchResultDecoder =
+    Json.Decode.Pipeline.decode SearchResult
+        |> required "id" int
+        |> required "name" string
+        |> required "timeline" string
+        |> required "status" string
+        |> required "categories" string
+        |> required "technologies" string
+        |> required "link" string
+        |> required "src_link" string
+        |> required "description" string
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+        [ header []
+            [ h1 [] [ text "ElmHub" ]
+            , span [] [ text "Like GitHub, but for Elm things." ]
             ]
-        , h3 []
-            [ text ("Active") ]
         , ul []
-            [ li []
-                [ a
-                    [ href "https://list-o-matic.herokuapp.com/"
-                    , target "_blank"
-                    ]
-                    [ text ("link") ]
-                , text (" - A web app that lets users log in and contribute to ranked lists of albums. The very-relatable")
-                ]
-            , li []
-                [ a
-                    [ href "https://soundcloud.com/sounds-park"
-                    , target "_blank"
-                    ]
-                    [ text ("Sounds Park") ]
-                , text (" - Intermittently updated with songs and remixes of my creation.")
-                ]
-            ]
+            (List.map viewSearchResult model.results)
         ]
+
+
+viewSearchResult : SearchResult -> Html Msg
+viewSearchResult result =
+    li []
+        [ text "Anything" ]
+
+
+type Msg
+    = SetQuery String
+
+
+
+-- update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    initialModel
